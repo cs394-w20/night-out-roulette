c585185dc24d2d6542f358c42a9b01e56a63b5ee
Rishi Gudivaka <rishi.gudivaka@gmail.com>
2020-02-17 12:17:12 -0600

    fixed conflict

--- nor-native-app/components/Choose.js
+++ nor-native-app/components/Choose.js
@@ -1,127 +1,134 @@
 const data = [
-    {
-        "name": "Dave's New Kitchen",
-        "cuisine": "Italian",
-        "price": "$$"
-    },
-    {
-        "name": "Soban Korea",
-        "cuisine": "Korean",
-        "price": "$$"
-    },
-    {
-        "name": "Tomate Fresh Kitchen",
-        "cuisine": "Mexican",
-        "price": "$"
-    },
-    {
-        "name": "Burger King",
-        "cuisine": "American",
-        "price": "$"
-    },
-    {
-        "name": "The Barn",
-        "cuisine": "Steak",
-        "price": "$$$$"
-    },
-    {
-        "name": "Potbelly's",
-        "cuisine": "American,Sandwich",
-        "price": "$$"
-    },
-    {
-        "name": "Chipotle",
-        "cuisine": "Mexican",
-        "price": "$"
-    },
-    {
-        "name": "Andy's Frozen Custard",
-        "cuisine": "Dessert",
-        "price": "$$"
-    },
-    {
-        "name": "Bat 17",
-        "cuisine": "American",
-        "price": "$$$"
-    },
-    {
-        "name": "Taco Diablo",
-        "cuisine": "Mexican",
-        "price": "$$$"
-    },
-    {
-        "name": "Clarke's",
-        "cuisine": "Breakfast",
-        "price": "$$"
-    },
-    {
-        "name": "Le Peep",
-        "cuisine": "Breakfast",
-        "price": "$$"
-    },
-    {
-        "name": "Nobu",
-        "cuisine": "Japanese",
-        "price": "$$$$"
-    },
-    {
-        "name": "Todoroki",
-        "cuisine": "Japanese",
-        "price": "$$$"
-    }
-]
+  {
+    name: "Dave's New Kitchen",
+    cuisine: "Italian",
+    price: "$$"
+  },
+  {
+    name: "Soban Korea",
+    cuisine: "Korean",
+    price: "$$"
+  },
+  {
+    name: "Tomate Fresh Kitchen",
+    cuisine: "Mexican",
+    price: "$"
+  },
+  {
+    name: "Burger King",
+    cuisine: "American",
+    price: "$"
+  },
+  {
+    name: "The Barn",
+    cuisine: "Steak",
+    price: "$$$$"
+  },
+  {
+    name: "Potbelly's",
+    cuisine: "American,Sandwich",
+    price: "$$"
+  },
+  {
+    name: "Chipotle",
+    cuisine: "Mexican",
+    price: "$"
+  },
+  {
+    name: "Andy's Frozen Custard",
+    cuisine: "Dessert",
+    price: "$$"
+  },
+  {
+    name: "Bat 17",
+    cuisine: "American",
+    price: "$$$"
+  },
+  {
+    name: "Taco Diablo",
+    cuisine: "Mexican",
+    price: "$$$"
+  },
+  {
+    name: "Clarke's",
+    cuisine: "Breakfast",
+    price: "$$"
+  },
+  {
+    name: "Le Peep",
+    cuisine: "Breakfast",
+    price: "$$"
+  },
+  {
+    name: "Nobu",
+    cuisine: "Japanese",
+    price: "$$$$"
+  },
+  {
+    name: "Todoroki",
+    cuisine: "Japanese",
+    price: "$$$"
+  }
+];
 
 function GetRestaurant(cuisine, price) {
-    let options = [];
-    console.log(cuisine)
-    console.log(price)
+  let options = [];
+  console.log(cuisine);
+  console.log(price);
 
-    for(let i = 0; i < data.length; i++){
-        if (data[i].cuisine.includes(cuisine) === true && data[i].price === price){
-            options.push(i);
-        }
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].cuisine.includes(cuisine) === true && data[i].price === price) {
+      options.push(i);
     }
+  }
 
-    const randomNum = Math.floor(Math.random() * options.length);
-    console.log(options)
+  const randomNum = Math.floor(Math.random() * options.length);
+  console.log(options);
 
-    return data[options[randomNum]];
+  return data[options[randomNum]];
 }
 
-const GetRestaurantYelp = async(cuisine, price, term = "food", radius = "10000", latitude = 42.057840, longitude = -87.676140, open_now = true) => {
+export default async function GetRestaurantYelp(
+  cuisine,
+  price,
+  term = "food",
+  radius = "10000",
+  latitude = 42.05784,
+  longitude = -87.67614,
+  open_now = true
+) {
+  queryString = "https://api.yelp.com/v3/businesses/search?";
 
-    queryString = "https://api.yelp.com/v3/businesses/search?"
+  // API
+  queryString = queryString + "Authorization=" + process.env.REACT_APP_API_KEY;
 
-    // API
-    queryString = queryString + "Authorization=" + process.env.REACT_APP_API_KEY;
+  // TERM
+  queryString = queryString + "&term=" + term;
 
-    // TERM
-    queryString = queryString + "&term=" + term;
+  // LATITUDE
+  queryString = queryString + "&latitude=" + latitude;
 
-    // LATITUDE
-    queryString = queryString + "&latitude=" + latitude;
+  // LONGITUDE
+  queryString = queryString + "&longitude=" + longitude;
 
-    // LONGITUDE
-    queryString = queryString + "&longitude=" + longitude;
-    
-    // RADIUS
-    queryString = queryString + "&radius=" + radius;
+  // RADIUS
+  queryString = queryString + "&radius=" + radius;
 
-    // CATEGORIES
-    queryString = queryString + "&categories=" + cuisine.toLowerCase();
+  // CATEGORIES
+  queryString = queryString + "&categories=" + cuisine.toLowerCase();
 
-    // PRICE
-    queryString = queryString + "&price=" + price.length;
+  // PRICE
+  queryString = queryString + "&price=" + price.length;
 
-    // OPEN NOW
-    queryString = queryString + "&open_now=" + open_now;
+  // OPEN NOW
+  queryString = queryString + "&open_now=" + open_now;
 
-    
-    let responseData = [];
-
-    fetch(queryString)
+  let responseData = [];
+  console.log("made it 1");
+  fetch(queryString)
     .then(response => response.json())
     .then(response => {
-        console.log(response)
-    })
+      console.log("made it 2");
+      console.log(response);
+    });
 }
