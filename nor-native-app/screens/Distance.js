e12f258900f7b37910c435aaa74edb831e741aff
david dlott <dmdlott@gmail.com>
2020-02-18 11:30:16 -0600

    cloned price page as distance page

---
+++ nor-native-app/screens/Distance.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, Picker } from "react-native";
+
+export default function Price({ navigation, route }) {
+  const [state, setstate] = useState({ price: "$", cuisine: route.params.cuisine });
+  console.log(state)
+  return (
+    <View style={styles.container}>
+      <View style={styles.instructions}>
+        <Text style={styles.text}>What's your budget?</Text>
+      </View>
+      <View style={styles.picker}>
+        <Picker
+          style={styles.picker}
+          selectedValue={state.price}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            setstate({...state, price: itemValue })
+          }
+        >
+          <Picker.Item label="$" value="$" />
+          <Picker.Item label="$$" value="$$" />
+          <Picker.Item label="$$$" value="$$$" />
+          <Picker.Item label="$$$$" value="$$$$" />
+        </Picker>
+      </View>
+      <View style={styles.button}>
+        <Button title="Next" onPress={() => navigation.navigate("Roulette", {...state})} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  instructions: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 20
+  },
+  picker: {
+    flex: 3,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  button: {
+    flex: 1
+  }
+});
