db05eba2f2da6c258b511ede8cea3812df2a8778
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-02-18 18:58:45 -0600

    Fixed yelp link, added maps routine, fixed onClick -> onPress

--- nor-native-app/screens/Roulette.js
+++ nor-native-app/screens/Roulette.js
@@ -1,25 +1,60 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
-import GetRestaurant from '../components/Choose'
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, Image, Linking, Platform } from "react-native";
+import { REACT_APP_API_KEY } from "react-native-dotenv";
 
 export default function Roulette({ navigation, route }) {
   const [displayRestaurant, setDisplayRestaurant] = useState(false);
-  return (
-    <View>
-      <Button title="Where's My Night Out?" display={displayRestaurant ? "none" : "block"} onClick={() => setDisplayRestaurant(true)}>Where's My Night Out?</Button>
-      <View display={displayRestaurant ? "block" : "none"} style={styles.container}>
-        <Text>{GetRestaurant(route.params.cuisine, route.params.price)['name']}</Text>
-        <Image
-          source={{
-            uri:
-              "https://media-cdn.tripadvisor.com/media/photo-s/14/f0/10/b7/photo0jpg.jpg"
-          }}
-          style={{ width: 400, height: 400 }}
-        />
+  const [restaurant, setRestaurant] = useState(null);
+
+  useEffect(() => {
+    var cuisine = route.params.cuisine;
+    var price = route.params.price;
+    let queryString = formQuery(cuisine, price)
+    let responseData = [];
+    fetch(queryString, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + REACT_APP_API_KEY
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+      reponseData = response.businesses;
+
+      const randomNum = Math.floor(Math.random() * responseData.length);
+
+      const chosenRestaurant = reponseData[randomNum];
+
+      console.log(chosenRestaurant);
+      setRestaurant(chosenRestaurant)
+    });
+  }, []);
+
+  if(!displayRestaurant || !restaurant) {
+    return (
+      <View style={styles.container}>
+        <Button title= "Where's My Night Out?" onPress={() => setDisplayRestaurant(!displayRestaurant)}></Button>
       </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>{restaurant['name']}</Text>
+      <Image
+        source={{
+          uri:
+            restaurant['image_url']
+        }}
+        style={{ width: 400, height: 400 }}
+      />
+      <Button title="Take me there!" onPress={() => {var address = restaurant['location']['display_address'][0].replace(' ', '+') + restaurant['location']['display_address'][1].replace(' ', '+'); 
+                                                      Linking.openURL(Platform.select({ ios: 'maps:0,0?q='+address, android: 'geo:0,0?q='+address }))}}>
+      </Button>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -28,3 +63,28 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   }
 });
+
+function formQuery(
+  cuisine,
+  price,
+  term = "food",
+  radius = "40000",
+  limit = 5,
+  latitude = 42.05784,
+  longitude = -87.67614,
+  open_now = true
+) {
+  let queryString = "https://api.yelp.com/v3/businesses/search?";
+
+  queryString += ("term=" + term);
+  queryString += ("&latitude=" + latitude);
+  queryString += ("&longitude=" + longitude);
+  queryString += ("&radius=" + radius);
+  queryString += ("&limit=" + limit);                         // LIMIT OF NUMBER OF RESTAURANTS
+  queryString += ("&categories=" + cuisine.toLowerCase());
+  queryString += ("&price=" + price.length);
+  queryString += ("&open_now=" + open_now); 
+
+  return queryString;
+  
+}
